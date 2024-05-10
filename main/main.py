00456f6b3569b4777e3eed2ac5d2d89035101579
EtoKekyes <kirill040402@yandex.ru>
2024-05-10 03:49:08 +0200

    `12`12

--- main/main.py
+++ main/main.py
@@ -10,15 +10,10 @@ packet_counts = Counter()
 ##detect WEP AP
 
 def detect_wep_ap(packet):
-    #while True:
+    while True:
         if packet.haslayer(Dot11WEP): # type: ignore
             print('[' + now.strftime("%Y/%m/%d %I:%M:%S") + ']' + ' WEP AP detected, packet sent from ' + str(packet.addr1).swapcase() +' to ' + str(packet.addr2).swapcase(), end='\r')
-        #print(packet.summary())
-        key = tuple(sorted([packet[0][1].src, packet[0][1].dst]))
-        packet_counts.update([key])
-        return f"Packet #{sum(packet_counts.values())}: {packet[0][1].src} ==> {packet[0][1].dst}"
 sniff(iface="wlan0", prn=detect_wep_ap, store=False, count=0)
-print("\n".join(f"{f'{key[0]} <--> {key[1]}'}: {count}" for key, count in packet_counts.items()))
 
 
 #counter = 0
