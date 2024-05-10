d4ad563f8155c4f07ef927feee2988731c405701
EtoKekyes <kirill040402@yandex.ru>
2024-05-10 03:09:39 +0200

    `122`

--- main/main.py
+++ main/main.py
@@ -5,18 +5,24 @@ import subprocess
 
 now = datetime.datetime.now()
 
-#detect WEP AP
-#counter = 0
+##detect WEP AP
 
+#counter = 0
 #while byte:
- #   print(byte)
-  #  byte = file1.read(1)
-   # counter += 1
+#    print(byte)
+#    byte = file1.read(1)
+#    counter += 1
 
 #print(counter)
+
+
 def detect_wep_ap(packet):
-    if packet.haslayer(Dot11WEP):
-        print(' [ ' +  now.strftime("%Y/%m/%d %I:%M:%S") + ' ] '+  ' WEP AP detected      MAC: ' +   str(packet.addr1).swapcase() + 'sent to' + str(packet.addr2).swapcase())
+    if packet.haslayer(Dot11WEP): # type: ignore
+        print('[' + now.strftime("%Y/%m/%d %I:%M:%S") + ']' + ' WEP AP detected, packet sent from MAC: ' + str(packet.addr1).swapcase() +' to MAC ' + str(packet.addr2).swapcase())
         print(packet.summary())
     return
 sniff(iface="wlan0", prn=detect_wep_ap, store=False, count=0)
+
+# len(packet)
+# for i in packet:
+#     print i.summary()
