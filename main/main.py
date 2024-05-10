c547d6b3f8d99ef3af987cda788875eaa93873a7
EtoKekyes <kirill040402@yandex.ru>
2024-05-10 15:28:56 +0200

    12121

--- main/main.py
+++ main/main.py
@@ -6,17 +6,24 @@ from collections import Counter
 
 now = datetime.datetime.now() #date and time
 
-def scan(packet):
+def wep(packet):
         if packet.haslayer(Dot11WEP):
-            print('[' + now.strftime("%Y/%m/%d %I:%M:%S") + ']' + ' WEP AP detected, packet sent from WEP AP ' + str(packet.addr1).swapcase() +' to device ' + str(packet.addr2).swapcase())
-        if packet.haslayer(Dot11Deauth):
-            print('\n'+'[' + now.strftime("%Y/%m/%d %I:%M:%S") + ']' + ' Deauthentication attack detected, packet sent from ' + str(packet.addr1).swapcase() +' to device ' + str(packet.addr2).swapcase(), end = "/r")
-        if packet.haslayer(Dot11FCS):
-            print('[' + now.strftime("%Y/%m/%d %I:%M:%S") + ']' + ' PS-Poll attack detected, packet sent from ' + str(packet.addr1).swapcase() +' to device ' + str(packet.addr2).swapcase())
+            output = ('[' + now.strftime("%Y/%m/%d %I:%M:%S") + ']' + ' WEP AP detected, packet sent from WEP AP ' + str(packet.addr1).swapcase() +' to device ' + str(packet.addr2).swapcase())
+            print(output, end="\r")
         return
-sniff(iface="wlan0", prn=scan, store=False, count=0)
+sniff(iface="wlan0", prn=wep, store=False, count=0)
 
+def deauth(packet):
+    if packet.haslayer(Dot11Deauth):
+        print('[' + now.strftime("%Y/%m/%d %I:%M:%S") + ']' + ' Deauthentication attack detected, packet sent from ' + str(packet.addr1).swapcase() +' to device ' + str(packet.addr2).swapcase())
+    return
+sniff(iface="wlan0", prn=deauth, store=False, count=0)
 
+def pspoll(packet):
+    if packet.haslayer(Dot11FCS):
+        print('[' + now.strftime("%Y/%m/%d %I:%M:%S") + ']' + ' PS-Poll attack detected, packet sent from ' + str(packet.addr1).swapcase() +' to device ' + str(packet.addr2).swapcase())
+    return
+sniff(iface="wlan0", prn=pspoll, store=False, count=0)
 
 #counter = 0
 #while byte:
