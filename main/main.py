6d73b9f10a9cd6edb95c8656ed1af54bf244705a
EtoKekyes <kirill040402@yandex.ru>
2024-05-10 03:59:41 +0200

    tewst

--- main/main.py
+++ main/main.py
@@ -7,12 +7,25 @@ from collections import Counter
 now = datetime.datetime.now() #date and time
 packet_counts = Counter()
 
-##detect WEP AP
+##detect WEP Access Points
 
 def detect_wep_ap(packet):
-    while True:
         if packet.haslayer(Dot11WEP): # type: ignore
-            print('[' + now.strftime("%Y/%m/%d %I:%M:%S") + ']' + ' WEP AP detected, packet sent from ' + str(packet.addr1).swapcase() +' to ' + str(packet.addr2).swapcase(), end='\r')
+            print('[' + now.strftime("%Y/%m/%d %I:%M:%S") + ']' + ' WEP AP detected, packet sent from WEP AP ' + str(packet.addr1).swapcase() +' to device ' + str(packet.addr2).swapcase())
+sniff(iface="wlan0", prn=detect_wep_ap, store=False, count=0)
+
+##detect Deauthentication
+
+def detect_wep_ap(packet):
+        if packet.haslayer(Dot11Deauth): # type: ignore
+            print('[' + now.strftime("%Y/%m/%d %I:%M:%S") + ']' + ' Deauthentication attack detected, packet sent from ' + str(packet.addr1).swapcase() +' to device ' + str(packet.addr2).swapcase())
+sniff(iface="wlan0", prn=detect_wep_ap, store=False, count=0)
+
+##detect PS-Poll (Power Save Poll protocol)
+
+def detect_wep_ap(packet):
+        if packet.haslayer(Dot11FCS): # type: ignore
+            print('[' + now.strftime("%Y/%m/%d %I:%M:%S") + ']' + ' PS-Poll attack detected, packet sent from ' + str(packet.addr1).swapcase() +' to device ' + str(packet.addr2).swapcase())
 sniff(iface="wlan0", prn=detect_wep_ap, store=False, count=0)
 
 
