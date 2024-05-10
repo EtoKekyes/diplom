a15ae84e6fd17ca680a347a022490e8fbd11f8cf
EtoKekyes <kirill040402@yandex.ru>
2024-05-10 04:14:42 +0200

    123123

--- main/main.py
+++ main/main.py
@@ -9,24 +9,29 @@ packet_counts = Counter()
 
 ##detect WEP Access Points
 
-def wep(packet):
+def scan(packet):
         if packet.haslayer(Dot11WEP): # type: ignore
             print('[' + now.strftime("%Y/%m/%d %I:%M:%S") + ']' + ' WEP AP detected, packet sent from WEP AP ' + str(packet.addr1).swapcase() +' to device ' + str(packet.addr2).swapcase())
-sniff(iface="wlan0", prn=wep, store=False, count=0)
-
-##detect Deauthentication
-
-def deauth(packet):
         if packet.haslayer(Dot11Deauth): # type: ignore
             print('[' + now.strftime("%Y/%m/%d %I:%M:%S") + ']' + ' Deauthentication attack detected, packet sent from ' + str(packet.addr1).swapcase() +' to device ' + str(packet.addr2).swapcase())
-sniff(iface="wlan0", prn=deauth, store=False, count=0)
-
-##detect PS-Poll (Power Save Poll protocol)
-
-def pspoll(packet):
         if packet.haslayer(Dot11FCS): # type: ignore
             print('[' + now.strftime("%Y/%m/%d %I:%M:%S") + ']' + ' PS-Poll attack detected, packet sent from ' + str(packet.addr1).swapcase() +' to device ' + str(packet.addr2).swapcase())
-sniff(iface="wlan0", prn=pspoll, store=False, count=0)
+        return
+sniff(iface="wlan0", prn=scan, store=False, count=0)
+
+# ##detect Deauthentication
+
+# def deauth(packet):
+#         if packet.haslayer(Dot11Deauth): # type: ignore
+#             print('[' + now.strftime("%Y/%m/%d %I:%M:%S") + ']' + ' Deauthentication attack detected, packet sent from ' + str(packet.addr1).swapcase() +' to device ' + str(packet.addr2).swapcase())
+# sniff(iface="wlan0", prn=deauth, store=False, count=0)
+
+# ##detect PS-Poll (Power Save Poll protocol)
+
+# def pspoll(packet):
+#         if packet.haslayer(Dot11FCS): # type: ignore
+#             print('[' + now.strftime("%Y/%m/%d %I:%M:%S") + ']' + ' PS-Poll attack detected, packet sent from ' + str(packet.addr1).swapcase() +' to device ' + str(packet.addr2).swapcase())
+# sniff(iface="wlan0", prn=pspoll, store=False, count=0)
 
 
 #counter = 0
