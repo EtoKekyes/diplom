7c266accfcb24efc820e9a899346e846562f7c82
EtoKekyes <kirill040402@yandex.ru>
2024-05-17 05:09:58 +0200

    123

--- main.py
+++ main.py
@@ -2,35 +2,34 @@
 
 from dataclasses import dataclass, fields
 from scapy.all import sniff, Packet, Dot11, Dot11WEP, Dot11FCS, Dot11Deauth, Dot11Disas
-from datetime import datetime
+from datetime import date, datetime
 
-now = datetime.now()
+now = datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
 
 def wep(pkt: Packet):
     if pkt.haslayer(Dot11WEP):
         f = open("stats.txt", "a")
-        print("[",now,"]",f"WEP AP detected with MAC: {pkt.addr1}", file=f)
-        print("[",now,"]",f"WEP AP detected with MAC: {pkt.addr1}")
+        print("[",now,"]",f"WEP AP detected with MAC: {pkt.addr1}", file=f, sep="")
+        print("[",now,"]",f"WEP AP detected with MAC: {pkt.addr1}", sep="")
 
 def pspoll(pkt: Packet):
     #if pkt.haslayer(Dot11FCS):
     if pkt.type==1 and pkt.subtype==10:
         f = open("stats.txt", "a")
-        print("[",now,"]",f"PS-Poll attack detected, packet sent from {pkt.addr1} to device {pkt.addr2}", file = f)
-        print("[",now,"]",f"PS-Poll attack detected, packet sent from {pkt.addr1} to device {pkt.addr2}")
-
+        print("[",now,"]",f"PS-Poll attack detected, packet sent from {pkt.addr1} to device {pkt.addr2}", file = f, sep="")
+        print("[",now,"]",f"PS-Poll attack detected, packet sent from {pkt.addr1} to device {pkt.addr2}", sep="")
 
 def deauth(pkt: Packet):
     if pkt.haslayer(Dot11Deauth):
         f = open("stats.txt", "a")
-        print("[",now,"]",f'DoS Deauthentication attack detected, packet sent from {pkt.addr1} to device {pkt.addr2}', file = f)
-        print("[",now,"]",f'DoS Deauthentication attack detected, packet sent from {pkt.addr1} to device {pkt.addr2}')
+        print("[",now,"]",f'DoS Deauthentication attack detected, packet sent from {pkt.addr1} to device {pkt.addr2}', file = f, sep="")
+        print("[",now,"]",f'DoS Deauthentication attack detected, packet sent from {pkt.addr1} to device {pkt.addr2}', sep="")
 
 def disas(pkt: Packet):
     if pkt.type==0 and pkt.subtype==12:
         f = open("stats.txt", "a")
-        print("[",now,"]",f'DoS Disassociation attack detected, packet sent from {pkt.addr1} to device {pkt.addr2}', file=f)
-        print("[",now,"]",f'DoS Disassociation attack detected, packet sent from {pkt.addr1} to device {pkt.addr2}')
+        print("[",now,"]",f'DoS Disassociation attack detected, packet sent from {pkt.addr1} to device {pkt.addr2}', file=f, sep="")
+        print("[",now,"]",f'DoS Disassociation attack detected, packet sent from {pkt.addr1} to device {pkt.addr2}', sep="")
 
 def stats():
     f = open("stats.txt")
@@ -39,11 +38,12 @@ def stats():
     pspoll = contents.count("PS-Poll")
     disas = contents.count("Disassociation")
     deauth = contents.count("Deauthentication")
-    print("Current stats:""\n"
-          "PS-Poll attacks detected = ",pspoll,"\n"
-          "DoS Deauthentication attacks detected = ",deauth, "\n"
-          "DoS Disassociation attacks detected = ",disas, "\n"
-          "WEP APs detected = ",wep, "last known attack was ", now " ago")
+    print("|-------------------------------------------------------------------------------|\n"
+          "| Current stats:""\n"
+          "| PS-Poll attacks detected =",pspoll, "\n"
+          "| DoS Deauthentication attacks detected =",deauth, "\n"
+          "| DoS Disassociation attacks detected =",disas, "\n"
+          "| WEP APs detected =",wep, "last known attack was at", now)
     f.close()
 
 @dataclass
---
+++ tdelta.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+
+#f = open("stats.txt")
+#contents = f.read()
+# wep = contents.count("WEP AP")
+# pspoll = contents.count("PS-Poll")
+# disas = contents.count("Disassociation")
+#deauth = contents.count("Deauthentication")
+# print("|-------------------------------------------------------------------------------|\n"
+#       "| Current stats:""\n"
+#       "| PS-Poll attacks detected =",pspoll, "\n"
+#       "| DoS Deauthentication attacks detected =",deauth, "\n"
+#       "| DoS Disassociation attacks detected =",disas, "\n"
+#       "| WEP APs detected =",wep, "last known attack was at")
+
+
+# Read dates from the file
+f = open("stats.txt")
+dates = [line.strip() for line in f]
+
+# Convert dates to datetime objects
+date_objects = [datetime.strptime(date, '%Y-%m-%d %H:%M:%S') for date in dates]
+
+# Find the latest date
+latest_date = max(date_objects)
+print("The latest date is:", latest_date)
